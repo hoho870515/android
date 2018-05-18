c6d3acdb8d7dcf2a4ba1f90867a61ac69b093687
D0543027 <a0935999309@gmail>
2018-05-18 19:47:59 +0800

    合併

--- d0543027/app/src/main/java/com/example/user/searchui/SearchActivity.java
+++ d0543027/app/src/main/java/com/example/user/searchui/SearchActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.searchui;
 
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,20 +13,21 @@ import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends ListActivity {
     private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -38,6 +40,7 @@ public class SearchActivity extends AppCompatActivity {
 
         Spinner spinner = (Spinner) findViewById(R.id.spin);
         //建立一個ArrayAdapter物件，並放置下拉選單的內容
+
         String[] str = {"","台中市","彰化縣","南投縣","雲林縣","苗栗縣"};
         final ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item,str);
         //設定下拉選單的樣式
@@ -45,34 +48,70 @@ public class SearchActivity extends AppCompatActivity {
         spinner.setAdapter(adapter);
         //設定項目被選取之後的動作
         spinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
+            ListView listview = (ListView) findViewById(android.R.id.list);
             public void onItemSelected(AdapterView adapterView, View view, int position, long id){
                 switch(position){
+                    case 0:
+                        ArrayList<String> clear = new ArrayList<>();
+                        clear.add(" ");
+                        clear.add(" ");
+                        clear.add(" ");
+                        clear.add(" ");
+                        clear.add(" ");
+                        ArrayAdapter arrayAdapterClear = new ArrayAdapter(SearchActivity.this,
+                                android.R.layout.simple_expandable_list_item_1,clear);
+                        listview.setAdapter(arrayAdapterClear);
+                        break;
                     case 1:
-                        Intent i = new Intent();
-                        i.setClass(SearchActivity.this, ListActivity.class);
-                        startActivity(i);
+                        ArrayList<String> str = new ArrayList<String>();
+                        str.add("搜尋結果1");
+                        str.add("搜尋結果2");
+                        str.add("搜尋結果3");
+                        str.add("搜尋結果4");
+                        str.add("搜尋結果5");
+                        ArrayAdapter arrayAdapter = new ArrayAdapter(SearchActivity.this,android.R.layout.simple_list_item_1, str);
+                        listview.setAdapter(arrayAdapter);
                         break;
                     case 2:
-                        Intent i1 = new Intent();
-                        i1.setClass(SearchActivity.this, ListActivity.class);
-                        startActivity(i1);
+                        ArrayList<String> str1 = new ArrayList<String>();
+                        str1.add("搜尋結果1");
+                        str1.add("搜尋結果2");
+                        str1.add("搜尋結果3");
+                        str1.add("搜尋結果4");
+                        str1.add("搜尋結果5");
+                        ArrayAdapter arrayAdapter1 = new ArrayAdapter(SearchActivity.this,android.R.layout.simple_list_item_1, str1);
+                        listview.setAdapter(arrayAdapter1);
                         break;
                     case 3:
-                        Intent i2 = new Intent();
-                        i2.setClass(SearchActivity.this, ListActivity.class);
-                        startActivity(i2);
+                        ArrayList<String> str2 = new ArrayList<String>();
+                        str2.add("搜尋結果1");
+                        str2.add("搜尋結果2");
+                        str2.add("搜尋結果3");
+                        str2.add("搜尋結果4");
+                        str2.add("搜尋結果5");
+                        ArrayAdapter arrayAdapter2 = new ArrayAdapter(SearchActivity.this,android.R.layout.simple_list_item_1, str2);
+                        listview.setAdapter(arrayAdapter2);
                         break;
                     case 4:
-                        Intent i3 = new Intent();
-                        i3.setClass(SearchActivity.this, ListActivity.class);
-                        startActivity(i3);
+                        ArrayList<String> str3 = new ArrayList<String>();
+                        str3.add("搜尋結果1");
+                        str3.add("搜尋結果2");
+                        str3.add("搜尋結果3");
+                        str3.add("搜尋結果4");
+                        str3.add("搜尋結果5");
+                        ArrayAdapter arrayAdapter3 = new ArrayAdapter(SearchActivity.this,android.R.layout.simple_list_item_1, str3);
+                        listview.setAdapter(arrayAdapter3);
                         break;
                     case 5:
-                        Intent i4 = new Intent();
-                        i4.setClass(SearchActivity.this, ListActivity.class);
-                        startActivity(i4);
+                        ArrayList<String> str4 = new ArrayList<String>();
+                        str4.add("搜尋結果1");
+                        str4.add("搜尋結果2");
+                        str4.add("搜尋結果3");
+                        str4.add("搜尋結果4");
+                        str4.add("搜尋結果5");
+                        ArrayAdapter arrayAdapter4 = new ArrayAdapter(SearchActivity.this,android.R.layout.simple_list_item_1, str4);
+                        listview.setAdapter(arrayAdapter4);
                         break;
-
                 }
 
             }
