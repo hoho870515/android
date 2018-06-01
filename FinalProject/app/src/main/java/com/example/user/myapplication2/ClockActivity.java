7c5b809f27571c3b59897cf247fcc8babc6e7a5c
D0543027 <a0935999309@gmail>
2018-06-01 22:13:48 +0800

    Add Final

---
+++ FinalProject/app/src/main/java/com/example/user/myapplication2/ClockActivity.java
@@ -0,0 +1,20 @@
+package com.example.user.myapplication2;
+
+import android.animation.AnimatorListenerAdapter;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.AnalogClock;
+import android.widget.DigitalClock;
+
+public class ClockActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.clock);
+
+        DigitalClock txtClock = (DigitalClock) findViewById(R.id.txtClock);
+        AnalogClock alclock = (AnalogClock) findViewById(R.id.alClock);
+    }
+}
--- FinalProject/app/src/main/java/com/example/user/myapplication2/SearchActivity.java
+++ FinalProject/app/src/main/java/com/example/user/myapplication2/SearchActivity.java
@@ -1,34 +1,28 @@
-package com.example.user.searchui;
+package com.example.user.myapplication2;
 
 import android.app.ListActivity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class SearchActivity extends ListActivity {
-    private Button btn;
+public class SearchActivity extends ListActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -37,15 +31,12 @@ public class SearchActivity extends ListActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
-
-        Spinner spinner = (Spinner) findViewById(R.id.spin);
-        //建立一個ArrayAdapter物件，並放置下拉選單的內容
-
+        });*/
         final String[] str = {"","台中市","彰化縣","南投縣","雲林縣","苗栗縣"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,str);
         //設定下拉選單的樣式
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner spinner = (Spinner) findViewById(R.id.spin);
         spinner.setAdapter(adapter);
         //設定項目被選取之後的動作
         spinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
@@ -121,25 +112,5 @@ public class SearchActivity extends ListActivity {
             }
         });
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_search, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
