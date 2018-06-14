3c8ba99dfd0f95f6a6d4d4f3db082ca31d8b4d3d
D0542664 <bonson930235@gmail.com>
2018-06-14 17:26:13 +0800

    新增ArrayList
    將資料已list儲存輸出

--- d0542664/app/src/main/java/com/example/bonso/txt/MainActivity.java
+++ d0542664/app/src/main/java/com/example/bonso/txt/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.bonso.txt;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ListView lv_notes;
+    SQLiteDatabase db;
+    ArrayList<String> titlelist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,14 +26,65 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, New.class);
-                startActivity(intent);
-                MainActivity.this.finish();
-            }
-        });
+        fab.setOnClickListener(addnote);
+
+        lv_notes = (ListView)findViewById(R.id.lv_notes);
+        lv_notes.setOnItemClickListener(iclick);
+        lv_notes.setOnItemLongClickListener(ilclick);
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        db.close();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        DBOpenHelper openhelper = new DBOpenHelper(this);
+        db = openhelper.getWritableDatabase();
+
+        titlelist = NoteDB.getTitleList(db);
+        ArrayAdapter<String> adapter =new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, titlelist);
+        lv_notes.setAdapter(adapter);
+    }
+
+    View.OnClickListener addnote = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent();
+            intent.setClass(MainActivity.this,New.class);
+            intent.putExtra("NOTEPOS", -1);
+            startActivity(intent);
+        }
+    };
+
+    AdapterView.OnItemClickListener iclick = new AdapterView.OnItemClickListener() {
+
+        @Override
+        public void onItemClick(AdapterView<?> av, View v,int position, long id) {
+            Intent intent = new Intent();
+            intent.setClass(MainActivity.this,New.class);
+            intent.putExtra("NOTEPOS", position);
+            startActivity(intent);
+        }
+    };
+
+    AdapterView.OnItemLongClickListener ilclick = new AdapterView.OnItemLongClickListener() {
+        @Override
+        public boolean onItemLongClick(AdapterView<?> av, View v,
+                                       int position, long id) {
+            String title = titlelist.get(position);
+            NoteDB.delNote(db, title);
+            titlelist = NoteDB.getTitleList(db);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                    (MainActivity.this,android.R.layout.simple_list_item_1, titlelist);
+            lv_notes.setAdapter(adapter);
+            return false;
+        }
+
+    };
 }
--- d0542664/app/src/main/java/com/example/bonso/txt/New.java
+++ d0542664/app/src/main/java/com/example/bonso/txt/New.java
@@ -1,11 +1,22 @@
 package com.example.bonso.txt;
 
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class New extends AppCompatActivity {
+
+    EditText title, note;
+    ArrayList<String> titlelist;
+    SQLiteDatabase db;
+    int notepos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +24,50 @@ public class New extends AppCompatActivity {
         setContentView(R.layout.activity_new);
         Spinner placeSpin = findViewById(R.id.placeSpinner);
         final String[] lunch = {"台中市","彰化縣","南投縣","雲林縣","苗栗縣"};
-        ArrayAdapter<String> lunchList = new ArrayAdapter<>(New.this, android.R.layout.simple_spinner_dropdown_item, lunch);
+        ArrayAdapter<String> lunchList = new ArrayAdapter<>(this,R.layout.myspinnertxt,lunch);
         placeSpin.setAdapter(lunchList);
+
+        title = (EditText)findViewById(R.id.Title);
+        note = (EditText)findViewById(R.id.Note);
+
+        Intent intent = getIntent();
+        notepos = intent.getIntExtra("NOTEPOS", -1);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        DBOpenHelper openhelper = new DBOpenHelper(this);
+        db = openhelper.getWritableDatabase();
+
+        titlelist = NoteDB.getTitleList(db);
+
+        if (notepos != -1) {
+            String printfTitle = titlelist.get(notepos);
+            title.setText(printfTitle);
+            note.setText(NoteDB.getBody(db,printfTitle));
+        } else {
+            title.setText("");
+            note.setText("");
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        String printfTitle = title.getText().toString();
+        if (printfTitle.length() == 0) {
+            Toast.makeText(this, "標題不能為空白，便條無儲存", Toast.LENGTH_SHORT).show();
+        } else {
+            NoteDB.addNote(db, title.getText().toString(),
+                    note.getText().toString());
+        }
+    }
+
+    boolean isTitleExist(String title) {
+        for (int i = 0; i < titlelist.size(); i++)
+            if (title.equalsIgnoreCase(titlelist.get(i)))
+                return true;
+        return false;
     }
 }
