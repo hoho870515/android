d63788b2dd26ae810e7d5179c54b7786d894cc48
D0542664 <bonson930235@gmail.com>
2018-06-19 22:34:59 +0800

    刪除多餘的fab

--- FinalProject/app/src/main/java/com/example/user/myapplication2/taichung_d.java
+++ FinalProject/app/src/main/java/com/example/user/myapplication2/taichung_d.java
@@ -13,17 +13,6 @@ public class taichung_d extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_taichung_d);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
