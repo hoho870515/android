49e7b7094e123b054f7299b7ed7de87d2abf8ebc
D0543027 <a0935999309@gmail.com>
2018-06-20 23:23:13 +0800

    整合時鐘

--- FinalProject/app/src/main/java/com/example/user/myapplication2/ClockActivity.java
+++ FinalProject/app/src/main/java/com/example/user/myapplication2/ClockActivity.java
@@ -1,13 +1,16 @@
 package com.example.user.myapplication2;
 
 import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.AnalogClock;
+import android.widget.Button;
 import android.widget.DigitalClock;
 
 public class ClockActivity extends AppCompatActivity {
-
+    private Button home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +19,18 @@ public class ClockActivity extends AppCompatActivity {
 
         DigitalClock txtClock = (DigitalClock) findViewById(R.id.txtClock);
         AnalogClock alclock = (AnalogClock) findViewById(R.id.alClock);
+        home = (Button)findViewById(R.id.button＿gohome);
+        home.setOnClickListener(gohome);
     }
+
+    private View.OnClickListener gohome = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            final int ACTIVITY_CLOCK = 1;
+            Intent intent = new Intent();
+            intent.setClass(ClockActivity.this, IndexActivity.class);
+            startActivityForResult(intent,ACTIVITY_CLOCK);
+        }
+    };
 }
